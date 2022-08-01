c117f17a3ff7b82fa6d37edf0da78de097b42a85
hector <hectoreddbenitez@gmail.com>
2022-08-01 15:53:26 -0300

    feat: motorcycle controller interface

---
+++ src/interfaces/IMotorcycleController.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default interface IMotorcycleController {
+  create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  read(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  readOne(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  update(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  delete(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+}
