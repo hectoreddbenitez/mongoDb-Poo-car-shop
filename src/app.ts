1da788c871ae6edffd842984d85a7e78911bdad8
hector <hectoreddbenitez@gmail.com>
2022-08-01 15:50:08 -0300

    feat: add motorcycle route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 import errorMiddleware from './middlewares/errorMiddlewares';
 import carRouter from './routes/CarRouter';
+import motorcycleRouter from './routes/MotorcycleRouter';
 
 const app = express();
-// import errorMiddleware from './middlewares/errorMiddleware';
-// import motoRouter from './routes/MotoRouter';
+
 app.use(express.json());
 app.use(carRouter);
+app.use(motorcycleRouter);
 app.use(errorMiddleware);
-
-// server.addMiddleware(motoRouter);
-// server.addMiddleware(errorMiddleware);
 
 export default app;
